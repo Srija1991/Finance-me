b8faaebd13bf898eadc73b5ea41b03a10612c861
Srija1991 <120301491+Srija1991@users.noreply.github.com>
2023-04-25 11:07:18 +0530

    vpc_removed

--- instance.tf
+++ instance.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "production-server" {
  ami = "ami-02eb7a4783e7e9317"
  instance_type = "t2.micro"
  availability_zone = "ap-south-1a"
- VPC_security_groups = [aws_security_group.sg-0c69f259b0ea97dc0.id]
  key_name = "dec29"
  tags = {
  name = "ansible_instance"
