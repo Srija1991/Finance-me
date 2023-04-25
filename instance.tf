f1eb1874755f3710311eca10b2a37b717462c487
Srija1991 <120301491+Srija1991@users.noreply.github.com>
2023-04-25 11:28:37 +0530

    key

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "production-server" {
  ami = "ami-02eb7a4783e7e9317"
  instance_type = "t2.micro"
  availability_zone = "ap-south-1a"
- key_name = "dec29"
+ key_name = "dec29.ppk"
  tags = {
  name = "ansible_instance"
  }
