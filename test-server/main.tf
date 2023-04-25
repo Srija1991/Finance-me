795659c9eb5db6a7d8823e3926f708cf553294e3
Srija1991 <120301491+Srija1991@users.noreply.github.com>
2023-04-25 15:54:28 +0530

    main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test-server" {
+  resource "aws_instance" "test-server" {
   ami           = "ami-02eb7a4783e7e9317" 
   instance_type = "t2.micro" 
   vpc_security_group_ids= ["sg-0c69f259b0ea97dc0"]
