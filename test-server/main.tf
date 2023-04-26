fb578ee770acc35617d00cebb43b8e4d0bdaf9af
Srija1991 <120301491+Srija1991@users.noreply.github.com>
2023-04-26 23:22:57 +0530

    Update main.tf

--- test-server/main.tf
+++ test-server/main.tf
@@ -16,9 +16,9 @@
     Name = "test-server"
   }
   provisioner "local-exec" {
-        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+    command = " echo ${aws_instance.test-server.public_ip} > inventory "
   }
   provisioner "local-exec" {
-        command = "ansible-playbook /var/lib/jenkins/workspace/Finance-me/test-server/finance-playbook.yml "
+    command = "ansible-playbook /var/lib/jenkins/workspace/Finance-me/test-server/finance-playbook.yml "
   } 
 }
